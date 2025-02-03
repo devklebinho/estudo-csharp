1527d708001adfc57c59f08ee330831d638427eb
ricsan95 <ricardo.ricsan95@gmail.com>
2025-02-03 17:34:23 -0300

    calculadora...

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,17 +1,53 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace Calculadora;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         Console.WriteLine("=========== CALCULADORA =============");
-        Console.WriteLine("Insira dois valores e escolha uma operação");
+        Console.WriteLine("Insira dois valores (X e Y) e escolha uma operação");
 
-        float num1, num2;
+        double num1, num2, result;
 
-        Console.WriteLine("");
+        Console.WriteLine("X: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Y: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(
+            "Escolha a operação:\n"+
+            "1 - Soma\n" +
+            "2 - Subtração\n" + 
+            "3 - Multiplicação\n" + 
+            "4 - Divisão\n");
+
+        string option = Console.ReadLine();
+          
+        switch(option)
+        {
+            case "1":
+                result = num1 + num2;
+                break;
+            case "2":
+                result = num1 - num2;
+                break;
+            case "3":
+                result = num1 * num2;
+                break;
+            case "4":
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Default");
+                break;
+            
+        }
         
+        Console.WriteLine("Resultado: " + result);
+
+        //Console.WriteLine("\nSoma: " + sum + "\nMultiplicação: " + mult + "\nSubtração: " + sub + "\nDivisão: " + div);
+
+        Console.ReadKey();
     }
 }
--- calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9358f3910f7438e191d5f041a55cebe45853ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+822ef516db4192acdc3cd25b8a1cadba0e74b1e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
