822ef516db4192acdc3cd25b8a1cadba0e74b1e7
Biblioteca <Biblioteca@D_013000.demprel.local>
2025-02-03 14:15:51 -0300

    iniciando projeto calculadora

---
+++ calculadora/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Calculadora;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("=========== CALCULADORA =============");
+        Console.WriteLine("Insira dois valores e escolha uma operação");
+
+        float num1, num2;
+
+        Console.WriteLine("");
+        
+    }
+}
