5c0be90e950730d6c9fb8be2f4015b4fa68abb53
devklebinho <contato@klebersilva.dev.br>
2025-02-10 16:03:28 -0300

    While and Do While

---
+++ while/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 0;
+        Console.WriteLine("\nIncrentando...");
+        while(i<=5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+        int j = 5;
+        Console.WriteLine("\nDecrentando...");        
+        while(j >= 0)
+        {
+            Console.WriteLine(j);
+            j--;
+        }
+    }
+}
