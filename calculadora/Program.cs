bc970999f5da142fc2e71b1daf56ba5f580a0fe4
Biblioteca <Biblioteca@D_013000.demprel.local>
2025-02-10 15:28:12 -0300

    Calculadora completed

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -5,49 +5,67 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("=========== CALCULADORA =============");
-        Console.WriteLine("Insira dois valores (X e Y) e escolha uma operação");
+        bool isProgramRunning = true;
 
-        double num1, num2, result;
-
-        Console.WriteLine("X: ");
-        num1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Y: ");
-        num2 = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine(
-            "Escolha a operação:\n"+
-            "1 - Soma\n" +
-            "2 - Subtração\n" + 
-            "3 - Multiplicação\n" + 
-            "4 - Divisão\n");
-
-        string option = Console.ReadLine();
-          
-        switch(option)
+        while(isProgramRunning)
         {
-            case "1":
+            Console.WriteLine("=========== CALCULADORA =============");
+            Console.WriteLine("Insira dois valores (X e Y) e escolha uma operação");
+
+            double num1, num2;
+
+            Console.WriteLine("X: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(
+                "Escolha a operação:\n"+
+                "1 - Soma\n" +
+                "2 - Subtração\n" + 
+                "3 - Multiplicação\n" + 
+                "4 - Divisão\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            double result = 0; 
+
+            if(option == 1)
+            {
                 result = num1 + num2;
-                break;
-            case "2":
+            }
+            else if(option == 2)
+            {
                 result = num1 - num2;
-                break;
-            case "3":
+            }
+            else if(option == 3)
+            {
                 result = num1 * num2;
-                break;
-            case "4":
-                result = num1 / num2;
-                break;
-            default:
-                Console.WriteLine("Default");
-                break;
-            
+            }
+            else if(option == 4)
+            {
+                if(num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Não é possível dividir um número por zero!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida");
+            }
+
+            Console.WriteLine("\nResultado: " + result + "\n");
+
+            Console.WriteLine("\nDeseja realizar outra operação (s/n)?");
+            string response = Console.ReadLine();
+            if(response != "s"){
+                isProgramRunning = false;
+            }
         }
-        
-        Console.WriteLine("Resultado: " + result);
 
-        //Console.WriteLine("\nSoma: " + sum + "\nMultiplicação: " + mult + "\nSubtração: " + sub + "\nDivisão: " + div);
-
-        Console.ReadKey();
+        Console.WriteLine("Obrigado por usar a calculadra!");
     }
 }
--- calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ calculadora/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+822ef516db4192acdc3cd25b8a1cadba0e74b1e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1527d708001adfc57c59f08ee330831d638427eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
