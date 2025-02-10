5339b51cd5d2fb0233bb55d24086896bdb844b83
devklebinho <contato@klebersilva.dev.br>
2025-02-10 16:22:59 -0300

    for loop

---
+++ for/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula14;
+
+public class Program
+{
+    public static void Main()
+    {
+        /*
+        Console.WriteLine("=========== Tabuada de Multiplicação ===========");
+        Console.WriteLine("Digite um número para saber a tabuada: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(numero + " x " + i + " = " + numero * i);
+        }
+        */
+        Console.WriteLine("=========== Matriz de Multiplicação ===========");
+        
+        for(int i = 1; i <= 10; i++)
+        {
+            for(int j = 1; j <= 10; j++)
+            {
+                // O valor 5 representa quantos espaços terá entre cada valor impresso
+                Console.Write($"{i * j, 5}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
