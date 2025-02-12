573912cda30e10d77330812431c27c2cc34ab825
devklebinho <contato@klebersilva.dev.br>
2025-02-12 15:17:09 -0300

    TargetFramework fixed to net8.0

--- List/Program.cs
+++ List/Program.cs
@@ -4,7 +4,31 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Digite uma palavra: ");
+        string separator = "-------------------------";
+
+        //1. Declaração com inicialização de valores
+        List<string> fruits = new List<string>()
+        {
+            "Manga",
+            "Abacate",
+            "Pera"
+        };
         
+        //2. Acessar os valores
+        Console.WriteLine(fruits[2]);
+        Console.WriteLine(separator);
+
+
+        //3. Adicionar Elementos
+        fruits.Add("Limão");
+        fruits.Add("Laranja");
+
+        foreach(string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        
+        Console.WriteLine(separator);
+        //4. 
     }
 }
